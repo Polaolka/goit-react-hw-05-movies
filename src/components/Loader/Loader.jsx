fe0c271cd3370c8ae43d6b85d1cfbf189cd819f7
Olga Polishchuk <olgapolichuk@ukr.net>
2023-03-31 12:40:02 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -13,7 +13,7 @@ export const Loader = () => {
           ariaLabel="blocks-loading"
           wrapperStyle={{}}
           wrapperClass="blocks-wrapper"
-          colors={['#b8c480', '#B2A3B5', '#F4442E', '#51E5FF', '#429EA6']}
+          colors={['#ff2045', '#8a6a90', '#b6000c', '#ff93b7', '#671332']}
         />
       </div>
   );
